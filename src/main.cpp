610910018deb658b1e28359e6e63612d1ac74719
Matheus Novais <mathpn2001@gmail.com>
2023-08-30 17:45:27 -0300

    adicionei o menu na main mesmo

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,47 @@
 
 #include <iostream>
 
+void menu() {
+    std::cout<<"Escolha o algoritmo a ser utizado da seguinte lista:"<<std::endl;
+    std::cout<<"1 -> FCFS (First Come, First Served)\n"
+               "2 -> Shortest Job First\n"
+               "3 -> Por prioridade, sem preempção \n"
+               "4 -> Por prioridade, com preempção por prioridade \n"
+               "5 -> Round-Robin com quantum = 2s, sem prioridade"<<std::endl;
+    
+    bool quebra = true;
+    do
+    {
+        std::cout<<"Sua escolha: "<<std::endl;
+        char choice;
+        std::cin >> choice;
+        
+        switch (choice)
+        {
+        case '1':
+            std::cout<<"Numero 1"<<std::endl;
+            quebra = false;
+            break;
+        case '2':
+            quebra = false;
+            break;
+        case '3':
+            quebra = false;
+            break;
+        case '4':
+            quebra = false;
+            break;
+        case '5':
+            quebra = false;
+            break;
+        default:
+            std::cout<<"Escolha um item da lista."<<std::endl;
+        }
+    } while (quebra);
+}
+
+
 int main(){
-    std::cout<<"hello world"<<std::endl;
+    menu();
     return 0;
 }
